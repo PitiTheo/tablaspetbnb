2c7668e3aa7435a77b55a9cb6c15cbf0ec526196
PitiTheo <julio.vilaboa@yandex.com>
2021-04-23 05:26:03 +0000

    empezando

--- src/models.py
+++ src/models.py
@@ -8,23 +8,49 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(Integer, nullable=False)
+    password = Column(String(30))
+    
+    
+
+class Owner(Base):
+    __tablename__ = 'owner'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    email = Column(String(30), unique=True, nullable=False)
+    pet_id = Column(Integer)
+   
+    
+class Persons(Base):
+    __tablename__ = 'persons'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30))
+    height = Column(Integer)
+    mass = Column(String(250))
+    hair_color = Column(String(30))
+    skin_color = Column(String(30))
+    eye_color = Column(String(30))
+    birth_year = Column(Integer)
+    gender = Column(String(30))
+
+class Planets(Base):
+    __tablename__ = 'planets' 
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(40))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    climate = Column(String(30))
+    gravity = Column(String(30))
+    terrain = Column(String(30))
+    population = Column(Integer)
+
 
     def to_dict(self):
         return {}
