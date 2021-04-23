e2f72a9d9df05bb711aeb6988e7382a6689514cc
PitiTheo <julio.vilaboa@yandex.com>
2021-04-23 21:52:04 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -14,14 +14,16 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    user_name = Column(Integer, nullable=False)
+    user_name = Column(String(30),unique=True, nullable=False)
+    last_name = Column(String(30),unique=False,nullable=False)
     birth_date = Column(Integer,unique=True, nullable=False)
     email = Column(String(30),unique=True, nullable=False)
     password = Column(String(30))
-    id_adress = Column(String(100), unique = False, nullable = False)
+    id_address = Column(String(100), unique = False, nullable = False)
     phone = Column(Integer,unique=True, nullable=False)
     user_rol = Column(Integer, unique = False, nullable = False)
     is_active = Column(Boolean(), unique=False, nullable=False)
+    
 
 
 
@@ -29,23 +31,36 @@ class Owner(Base):
     __tablename__ = 'owner'
     id = Column(Integer, primary_key=True)
     pet_id = Column(Integer)
+    user_id = Column(Integer, ForeignKey ('user.id'))
+    user_id = Column(Integer, ForeignKey ('comment.id'))
 
 class Buddy(Base):
     __tablename__ = 'buddy'
     id = Column(Integer, primary_key=True)
     service = Column(String(30))
+    user_id = Column(ForeignKey ('user.id'))
+    user_id = Column( ForeignKey ('comment.id'))
    
     
 class Address(Base):
     __tablename__ = 'address'
     id = Column(Integer, primary_key=True)
     id_comment = Column(String(200))
+    user_id = Column(Integer, ForeignKey ('user.id'))
+    
     
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_body= Column(String(200))
+
+class Pet(Base):
+    __tablename__ = 'pet'
+    id = Column(Integer, primary_key=True)
+    pet_id = Column(Integer)
+    pet_name = Column(String(30),unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey ('owner.id'))
 
 
     def to_dict(self):
